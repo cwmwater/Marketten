602cfbaa49a4becee1347e29b995f3415ea6295b
cwmwater <jo08198@gmail.com>
2025-10-02 18:43:32 +0900

    작업 내용

--- src/main/java/com/example/Marketten/controller/PostController.java
+++ src/main/java/com/example/Marketten/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.example.Marketten.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 @Slf4j
 public class PostController {
+
+    @GetMapping("/read")
+    public String read() {
+        return "read";
+    }
 }
